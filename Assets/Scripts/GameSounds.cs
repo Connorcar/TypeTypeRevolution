c8a77e7a3f20c2013a702dfac5a709dc58baf741
Connor Caruthers <cjcaruthers2@gmail.com>
2024-04-03 13:06:02 -0700

    Louder songs & pitchier notes
    - Normalized the song waveforms to make them louder
    - The pitches of the hit sound effects now correspond to the song playing

--- Assets/Scripts/GameSounds.cs
+++ Assets/Scripts/GameSounds.cs
@@ -15,29 +15,53 @@ using UnityEngine;
 
 public class GameSounds : MonoBehaviour
 {
+    private GameObject gameController;
+    private Game gc;
+
     public AudioSource hitSound1;
+
+    public float[] bayHitPitchValues = { 0.68f, 0.76f, 0.86f, 1.015f };
+    public float[] resolveHitPitchValues = { 0.75f, 0.84f, 0.94f, 1.13f };
+
+    private float[] currHitPitchValues = { };
+
+    private void Start()
+    {
+        gameController = GameObject.Find("GameController");
+        gc = gameController.GetComponent<Game>();
+
+        switch (gc.song_op)
+        {
+            case 0: // bay
+                currHitPitchValues = bayHitPitchValues;
+                break;
+            case 1: // resolve
+                currHitPitchValues = resolveHitPitchValues;
+                break;
+        }
+    }
 
     public void playOne()
     {
-        hitSound1.pitch = 0.68f;
+        hitSound1.pitch = currHitPitchValues[0];
         hitSound1.Play();
     }
 
     public void playTwo()
     {
-        hitSound1.pitch = 0.76f;
+        hitSound1.pitch = currHitPitchValues[1];
         hitSound1.Play();
     }
 
     public void playThree()
     {
-        hitSound1.pitch = 0.86f;
+        hitSound1.pitch = currHitPitchValues[2];
         hitSound1.Play();
     }
 
     public void playFour()
     {
-        hitSound1.pitch = 1.015f;
+        hitSound1.pitch = currHitPitchValues[3];
         hitSound1.Play();
     }
 }
